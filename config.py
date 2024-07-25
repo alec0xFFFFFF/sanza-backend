5ee3122c4555bbcc764ccd3e503a176ad3632108
Alec White <alexander.k.white@gmail.com>
2024-07-24 21:15:28 -0700

    update config

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # This loads the .env file
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://user:password@localhost/sanza_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STYTCH_PROJECT_ID = os.environ.get('STYTCH_PROJECT_ID')
     STYTCH_SECRET = os.environ.get('STYTCH_SECRET')

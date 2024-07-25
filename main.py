d31599bf0a511eb3e9d14b0cd02caf97415b377f
Alec White <alexander.k.white@gmail.com>
2024-07-24 21:59:14 -0700

    add welcome

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
+from flask import jsonify
 from app import create_app
 
 app = create_app()
+
+@app.route('/', methods=['GET'])
+def welcome():
+    return jsonify({"message": "Welcome to Sanza!"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True, port=os.environ.get("PORT", default=5000))
